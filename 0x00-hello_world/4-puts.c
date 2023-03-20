2feba99996204fb65e53ac01d4ac84bfccc5713e
Uyisenga <senga@gmail.com>
2023-03-20 07:59:48 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -a function that rwites puts function
+ *
+ * Return: always 0
+ */
+int mai(void)
+{
+puts ("\"Programming is like building a multilingual puzzle");
+return (0);
+
+
+}
