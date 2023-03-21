bf1ce6ef87b979548bd75fd9eb84ef3bdc7bc581
Uyisenga <senga@gmail.com>
2023-03-21 08:14:30 -0700

    print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that prints 10 times the alphabet
+ * Return: always (0);
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char b;
+
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (b = 'a' ; b <= 'z' ; b++)
+			_putchar(b);
+		_putchar('\n');
+
+	}
+
+
+
+
+
+}
+
+
