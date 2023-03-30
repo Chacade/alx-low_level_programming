cb696833efc25d422a69111ad5d6a7099b03884d
Uyisenga <senga@gmail.com>
2023-03-30 09:07:59 -0700

    function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/104-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char buffer[] = "string!\0A #buffer :)\1\2\3\4\5\6\7#cisfun\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34\x56#pointer #fun\n";
+
+	printf("%s\n", buffer);
+	printf("---------------------------------\n");
+	print_buffer(buffer, sizeof(buffer));
+	return (0);
+}
